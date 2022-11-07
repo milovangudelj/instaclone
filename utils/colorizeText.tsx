1f9f5cc1a4a74de405b8ef42352f3ed2e113c89f
milovangudelj <milovan.gudelj@gmail.com>
2022-11-07 21:28:44 +0100

    Guard?

--- utils/colorizeText.tsx
+++ utils/colorizeText.tsx
@@ -1,4 +1,6 @@
 export const colorizeText = (input: string): (string | JSX.Element)[] => {
+	if (!input) return [];
+	
 	return input.split(/([#|@][a-z0-9_]+)/g).map((segment, i) =>
 		segment.startsWith("#") || segment.startsWith("@") ? (
 			<span key={`cs_0${i}`} className="text-link">
