9c2a80da7662f35154d8136454856ea2dace42e4
milovangudelj <milovan.gudelj@gmail.com>
2022-11-07 21:32:59 +0100

    Again, nonsense

--- app/[user]/page.tsx
+++ app/[user]/page.tsx
@@ -119,34 +119,40 @@ const UserPage = async ({ params }: UserPageProps) => {
 							</Link>
 						</div>
 						<div className="mt-[12px] text-[12px] font-medium leading-[16px] text-dark-me">
-							Followed by{" "}
-							{followers.slice(0, 3).map((follower, i) => {
-								if (i === followers.length - 1)
-									return (
-										<>
-											{" "}
-											and{" "}
-											<span className="text-dark-he">
-												{follower}
-											</span>
-										</>
-									);
-								if (i === 2 && followers.length > 3)
-									return (
-										<>
-											<span className="text-dark-he">
-												{follower}
-											</span>{" "}
-											+ {followers.length - 3} more
-										</>
-									);
-								return (
-									<>
-										{i !== 0 && ", "}
-										<span className="text-dark-he">{follower}</span>
-									</>
-								);
-							})}
+							{followers && (
+								<>
+									Followed by{" "}
+									{followers.slice(0, 3).map((follower, i) => {
+										if (i === followers.length - 1)
+											return (
+												<>
+													{" "}
+													and{" "}
+													<span className="text-dark-he">
+														{follower}
+													</span>
+												</>
+											);
+										if (i === 2 && followers.length > 3)
+											return (
+												<>
+													<span className="text-dark-he">
+														{follower}
+													</span>{" "}
+													+ {followers.length - 3} more
+												</>
+											);
+										return (
+											<>
+												{i !== 0 && ", "}
+												<span className="text-dark-he">
+													{follower}
+												</span>
+											</>
+										);
+									})}
+								</>
+							)}
 						</div>
 					</div>
 				</div>
