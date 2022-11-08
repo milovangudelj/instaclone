cda3b1a39fef97a1debbea86acd99f931495246d
milovangudelj <milovan.gudelj@gmail.com>
2022-11-08 01:53:29 +0100

    New regex for hashtags and tags

--- utils/colorizeText.tsx
+++ utils/colorizeText.tsx
@@ -1,5 +1,5 @@
 export const colorizeText = (input: string): (string | JSX.Element)[] => {
-	return input.split(/([#|@][a-z0-9_]+)/g).map((segment, i) =>
+	return input.split(/([#|@][A-Za-z0-9_.]+)/g).map((segment, i) =>
 		segment.startsWith("#") || segment.startsWith("@") ? (
 			<span key={`cs_0${i}`} className="text-link">
 				{segment}
