a12a05211764e7138f01012aecf1aea73ffbba5f
milovangudelj <milovan.gudelj@gmail.com>
2022-11-09 00:31:32 +0100

    Open links in new tab

--- components/UserHeaderLink/UserHeaderLink.tsx
+++ components/UserHeaderLink/UserHeaderLink.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 export const UserHeaderLink = ({ link }: { link: string }) => {
 	return (
 		<div>
-			<Link href={link} className="font-semibold text-link hover:underline">
+			<Link
+				href={link}
+				target="_blank"
+				rel="noreferrer noopener"
+				className="font-semibold text-link hover:underline"
+			>
 				{link.replace(/(https?:\/\/)/g, "")}
 			</Link>
 		</div>
