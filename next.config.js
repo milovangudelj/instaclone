6960ec125fef90c99c00a1e4863bb918138d0487
milovangudelj <milovan.gudelj@gmail.com>
2023-09-21 23:51:33 +0200

    App router isn not experimental anymore

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ["scontent.cdninstagram.com", "instagram.fmxp5-1.fna.fbcdn.net"],
+    domains: ['scontent.cdninstagram.com', 'instagram.fmxp5-1.fna.fbcdn.net'],
   },
-};
+}
 
 module.exports = nextConfig;
